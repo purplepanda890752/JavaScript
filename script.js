117c2d7ca3eadafc8ace4a32bd5a3c46737570ed
purplepanda890752 <temu.lover230@gmail.com>
2024-10-05 20:35:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-function myFunction(){
-    var a = 785;
-    document.getElementById("demo").innerHTML=a*a
+//clear the calculator
+function clearscreen(){
+    document.getElementById("result").value="";
+}
+
+function setscreenvalue(value){
+    document.getElementById("result").value+=value;
+}
+
+function calculatoresult(){
+    const resultelement=document.getElementById("result");
+    const expression = resultelement.value.trim();
+
+    //check for empty input
+    if(expression==''){
+        resultelement.value='Enter an input';
+        return;
+    }
+    try{
+        resultelement.value=eval(expression);
+    }catch(e){
+        resultelement.value='Invalid input';
+    }
 }
